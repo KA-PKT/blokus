0bb1912dc51a7e29ca67d04af8fd760937eb67f1
KA-PKT <ccpockets64@gmail.com>
2021-10-04 02:27:42 +0900

    画面に盤面表示、ドラッグ&ドロップ動作の理解

--- lib/Block.dart
+++ lib/Block.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'common.dart';
 
 /// ブロック（Panelの集合体）のクラス
@@ -22,4 +23,66 @@ class Block {
     if (direction == Direction.Left) {
     } else if (direction == Direction.Right) {}
   }
+}
+
+Draggable createBlock(List<List<int>> shape) {
+  // TODO: ブロックの構成によって画像を調整する
+  var blockImg = Container(
+      margin: const EdgeInsets.all(1.0),
+      color: Colors.blue,
+      width: 400 / 14 * 2,
+      height: 400 / 14 * 2,
+      child: Column(children: <Widget>[
+        Expanded(
+          child: Container(
+            child: Row(children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(1.0),
+                  color: Colors.green,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(1.0),
+                  color: Colors.green,
+                ),
+              ),
+            ]),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            child: Row(children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(1.0),
+                  color: Colors.green,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(1.0),
+                  color: Colors.green,
+                ),
+              ),
+            ]),
+          ),
+        ),
+      ]));
+
+  return Draggable(
+    data: new Block(shape),
+    onDragStarted: () {},
+    onDraggableCanceled: (velocity, offset) {},
+    onDragCompleted: () {},
+    onDragEnd: (details) {},
+    child: blockImg,
+    feedback: blockImg,
+    childWhenDragging: Icon(
+      Icons.flag,
+      color: Colors.black.withOpacity(0),
+      size: 90,
+    ),
+  );
 }
